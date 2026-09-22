365140be8226501dd8703bab7e10e5c4b2cc700a
milad <m.moltaji@yahoo.com>
2026-09-22 21:05:13 +0330

    change debug

--- MenuApp/settings.py
+++ MenuApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!l20b4o457(^2-ly*a=(oc589jal_)-y#uj=hs9(62cf$m&osa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
